cd623871e270c6107f7d6ee1b3e36828586aad40
Superariel10 <ariel.orozco@ute.edu.ec>
2026-06-03 14:53:29 -0500

    completo

--- StartScreen/urls.py
+++ StartScreen/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
+from .auth_views import register_view
 
 router = DefaultRouter()
 router.register(r"generos", views.GeneroViewSet)
@@ -10,4 +12,7 @@ urlpatterns = [
     path("", include(router.urls)),
     path("cine/entradas/", views.calcular_entradas, name="calcular-entradas"),
     path("cine/sala/", views.programar_sala, name="programar-sala"),
+    path("auth/register/", register_view, name="auth-register"),
+    path("auth/login/", TokenObtainPairView.as_view(), name="auth-login"),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="auth-refresh"),
 ]
